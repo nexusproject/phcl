ebdfa66ffea5a288d7fc8bdf2e3810b60b73552f
Dmitry Sergeev <realnexusway@gmail.com>
2026-04-10 19:02:38 +0400

    Improving and fixing Declarative and Block

--- phcl/core/dsl.py
+++ phcl/core/dsl.py
@@ -1,25 +1,22 @@
-#
-#
-from pprint import pprint as p
-
-
 class Declarative:
     """
-    Base class for declarative constructs.
+    Declarative base.
     """
 
     @property
     def _phcl_attributes(self) -> dict:
         attrs = {}
 
-        for base in reversed(self.__class__.mro()):
+        for base in list(reversed(self.__class__.mro())) + [self]:
             if base is Declarative:
                 continue
 
             for name, value in base.__dict__.items():
                 if name.startswith("_"):
                     continue
-                if callable(value) and not isinstance(value, property):
+
+                # skip methods, keep classes and properties
+                if callable(value) and not isinstance(value, (property, type)):
                     continue
 
                 if isinstance(value, property):
@@ -31,11 +28,16 @@ class Declarative:
 
 
 def abstract(cls):
+    """Marks Node class as non-renderable."""
     cls.__phcl_abstract = True
     return cls
 
 
 class Registry(type):
+    """
+    Registers Node subclasses that should be rendered.
+    """
+
     _registry = []
 
     def __init__(self, *args):
@@ -46,12 +48,13 @@ class Registry(type):
             for parent in bases
             if hasattr(parent, "__phcl_type")
         ]
+
         if not phcl_types:
             return
 
         if len(phcl_types) > 1:
             raise Exception(
-                f"{name} cannot be derived from two or more Resource/Data types"
+                f"{name} cannot inherit multiple Resource/Data types"
             )
 
         setattr(self, "__phcl_label", name)
@@ -66,45 +69,49 @@ class Registry(type):
         ]
 
 
-class Node(Declarative, metaclass=Registry):
+class Renderable:
     """
-    Base AST node capable of recursive rendering.
+    Default render.
+    Recursive materialization of declarative values.
+    """
+
+    def _phcl_render_value(self, v):
+        if hasattr(v, "_phcl_render"):
+            v = v() if isinstance(v, type) else v
+            return v._phcl_render()
+
+        if isinstance(v, list):
+            return [self._phcl_render_value(x) for x in v]
+
+        if isinstance(v, dict):
+            return {k: self._phcl_render_value(x) for k, x in v.items()}
+
+        return v
+
+    def _phcl_render(self):
+        return {
+            k: self._phcl_render_value(v)
+            for k, v in self._phcl_attributes.items()
+        }
+
+
+class Node(Declarative, Renderable, metaclass=Registry):
+    """
+    Top-level PHCL entity.
     """
 
     def _get_identity(self):
         return getattr(self, "__phcl_type"), getattr(self, "__phcl_label")
 
 
-    def _phcl_render(self):
-        return {
-            k: v._phcl_render() if hasattr(v, "_phcl_render") else v
-            for k, v in self._phcl_attributes.items()
-        }
+class Block(Declarative, Renderable):
+    """
+    Nested block. Supports inheritance + ctor override.
+    """
 
-
-class Block(Declarative):
     def __init__(self, **kwargs):
-        self.__phcl_kwargs = kwargs
+        for k in kwargs:
+            if k.startswith("_"):
+                raise ValueError("Attributes starting with '_' are reserved")
 
-    def _render_value(self, v):
-        if hasattr(v, "_phcl_render"):
-            if isinstance(v, type):
-                v = v()
-            return v._phcl_render()
-
-        if isinstance(v, list):
-            return [self._render_value(x) for x in v]
-
-        if isinstance(v, dict):
-            return {k: self._render_value(x) for k, x in v.items()}
-
-        return v
-
-    def _phcl_render(self):
-        base = dict(getattr(type(self), "_phcl_attributes", {}))  # <-- ВОТ ЭТО
-        base.update(self.__phcl_kwargs)  # kwargs поверх
-
-        p(base)
-        return {k: self._render_value(v) for k, v in base.items()}
-
-
+        self.__dict__.update(kwargs)
